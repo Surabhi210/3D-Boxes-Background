6cc4bf47b432f4c160dab17e7a02f20f123b5033
Surabhi Sethi <surabhisethi210@gmail.com>
2025-10-31 10:04:36 +0530

    updated

--- script1.js
+++ script1.js
@@ -6,8 +6,8 @@ btn.addEventListener('click', () => boxesContainer.classList.toggle('big'))
 function createBoxes() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
-      const box = document.createElement('div')
-      box.classList.add('box')
+      const box = document.createElement('div') // Creates a new <div> element for each box.
+      box.classList.add('box') //Gives it the class box, which the CSS uses for styling.
       box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
       boxesContainer.appendChild(box)
     }
